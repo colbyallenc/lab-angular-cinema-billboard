84be192f3bbeea6a5daff3234c96c8e5cb963d9a
colbyallenc <colbyallenc@gmail.com>
2017-06-27 19:39:46 -0400

    minor addition

--- starter-code/src/app/my-movie/my-movie.component.ts
+++ starter-code/src/app/my-movie/my-movie.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CinemaMovies } from '../cinema.service'
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-my-movie',
   templateUrl: './my-movie.component.html',
-  styleUrls: ['./my-movie.component.css']
+  styleUrls: ['./my-movie.component.css'],
+  //providers:[CinemaService]
 })
 export class MyMovieComponent implements OnInit {
 
-  constructor() { }
+movieId: number;
+movie: Object;
 
-  ngOnInit() {
+ constructor(private cinema: CinemaMovies, private route: ActivatedRoute, private router:Router) { }
+
+ ngOnInit() {
+    this.route.params.subscribe((params)=> this.movieId = Number(params['id']));
+    this.movie = this.cinema.getMovie(this.movieId)
   }
 
 }
